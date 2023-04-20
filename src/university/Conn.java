0cce1c6f1969c2382a2ddc6ab6c906effca86f23
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-04-20 07:11:31 +0100

    Third Commit: Create skeleton Conn class

--- src/university/Conn.java
+++ src/university/Conn.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package university.management.information.system;
+package university;
 
 /**
  *
  * @author adrianadewunmi
  */
-public class UniversityManagementInformationSystem {
+public class Conn {
 
     /**
      * @param args the command line arguments
